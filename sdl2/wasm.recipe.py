331a87a4f32c49566c18ba1dd3f8663587badc82
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2019-07-11 20:59:49 +0200

    [sdl2] Remove uneeded params from wasm recipe

--- sdl2/wasm.recipe.py
+++ sdl2/wasm.recipe.py
@@ -2,7 +2,6 @@ from conans import ConanFile, CMake
 
 class RollGoalWasm(ConanFile):
     settings = {"os": ["Emscripten"]}
-    exports_sources = ["CMakeLists.txt", "src/*"]
     generators = ["cmake"]
 
     def _configure_cmake(self):
@@ -13,7 +12,3 @@ class RollGoalWasm(ConanFile):
     def build(self):
         cmake = self._configure_cmake()
         cmake.build()
-
-    def package(self):
-        cmake = self._configure_cmake()
-        cmake.install()
