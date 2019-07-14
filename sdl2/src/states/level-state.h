f444d0c3f425e812741062161f8f3c14727162bb
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2019-07-14 22:41:43 +0200

    [sdl] Fix push level state missing files

---
+++ sdl2/src/states/level-state.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "i-state.h"
+
+class LevelState : public IState {
+public:
+    LevelState();
+    virtual ~LevelState();
+
+    void onEnter();
+    void update();
+    void onExit();
+
+public:
+    GameState getName() const;
+};
