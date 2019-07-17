09ced9a126a837e711b29d9affbba2dfc2a7fc16
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2019-07-17 21:46:21 +0200

    [sdl] Add structure for input system basd on overwatch GDC

---
+++ sdl2/src/components/singleton/game-input.h
@@ -0,0 +1,6 @@
+/**
+ * @brief Stores button states as booleans, and store button bindings
+ * @link https://youtu.be/W3aieHjyNvw?t=715 - Overwatch Gameplay Architecture
+ */
+
+
---
+++ sdl2/src/systems/input-system.h
@@ -0,0 +1,3 @@
+/**
+ * @brief Get the singleton component GameInput and apply it to the command pattern implementation
+ */
