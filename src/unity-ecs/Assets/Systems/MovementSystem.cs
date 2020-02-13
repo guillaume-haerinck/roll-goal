9b430561cd993428b2b84ccd41e212cdf97acd7a
Guillaume <guillaume.haerinck@outlook.com>
2020-02-13 11:45:38 +0100

    [unity] Use new input system

--- src/unity-ecs/Assets/Systems/MovementSystem.cs
+++ src/unity-ecs/Assets/Systems/MovementSystem.cs
@@ -3,15 +3,17 @@ using Unity.Jobs;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
+using Unity.Physics;
+using Unity.Physics.Extensions;
 
 public class MovementSystem : JobComponentSystem
 {
     protected override JobHandle OnUpdate(JobHandle inputDeps)
     {
-        float deltaTime = Time.DeltaTime;
+        float dt = Time.DeltaTime;
 
-        JobHandle myJob = Entities.ForEach((ref Translation trans, in MotionComponent motion) => {
-            trans.Value = motion.direction * motion.speed;
+        JobHandle myJob = Entities.ForEach((ref PhysicsVelocity vel, in PhysicsMass mass, in MotionComponent motion) => {
+            ComponentExtensions.ApplyLinearImpulse(ref vel, mass, motion.direction * motion.speed * dt);
         }).Schedule(inputDeps);
 
         return myJob;
--- src/unity-ecs/Assets/Systems/PlayerInputSystem.cs
+++ src/unity-ecs/Assets/Systems/PlayerInputSystem.cs
@@ -2,15 +2,18 @@
 using Unity.Jobs;
 using Unity.Entities;
 using Unity.Mathematics;
+using UnityEngine.InputSystem;
 
 [AlwaysSynchronizeSystem]
 public class PlayerInputSystem : JobComponentSystem
 {
     protected override JobHandle OnUpdate(JobHandle inputDeps)
     {
-        Entities.ForEach((ref MotionComponent motion, in InputComponent input) => {
-            motion.direction.x += Input.GetKey(input.forwardKey) ? 1f : 0;
-            motion.direction.x -= Input.GetKey(input.backKey) ? 1f : 0;
+        float z = (Keyboard.current.upArrowKey.isPressed ? 1 : 0) + (Keyboard.current.downArrowKey.isPressed ? -1 : 0);
+        float x = (Keyboard.current.rightArrowKey.isPressed ? 1 : 0) + (Keyboard.current.leftArrowKey.isPressed ? -1 : 0);
+
+        Entities.ForEach((ref MotionComponent motion) => {
+            motion.direction = new float3(x, 0f, z);
         }).Run();
 
         return default;
